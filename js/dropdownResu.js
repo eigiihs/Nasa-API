d177d037bf71c791acd0250ce34af656231a305f
Sarah Cruz <sarinha.studiesllz@gmail.com>
2023-11-20 11:06:18 -0300

    continuação e conclusão do dropdown de resultados

--- js/dropdownResu.js
+++ js/dropdownResu.js
@@ -1,7 +1,10 @@
+// Seleciona todos os elementos com a classe "resultado"
 var elementosResultado = document.querySelectorAll('.resultado');
 
+// Para cada elemento com a classe "resultado", adiciona um ouvinte de evento de clique
 elementosResultado.forEach(function (resultado) {
+    // Quando um elemento com a classe "resultado" é clicado, é adicionado a classe "ativa"
     resultado.addEventListener('click', function() {
-        resultado.classList.add('')
+        resultado.classList.toggle('ativa');
     });
 });
