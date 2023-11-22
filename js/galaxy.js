58087062910a0635b0d2a1c836fe2fb142582e8f
laraazx <lara.zxnelato@gmail.com>
2023-11-22 14:32:41 -0300

    .

--- js/galaxy.js
+++ js/galaxy.js
@@ -1,20 +1,18 @@
 fetch("galaxias.json")
-    .then((result) => result.json())
-    .then((result) => {
+  .then((result) => result.json())
+  .then((result) => {
+    const galaxias = result.galaxias;
 
-        const galaxias = result.galaxias;
+    const randomGalaxy = galaxias.sort(() => Math.random() - 0.5);
 
-        const randomGalaxy = galaxias.sort(() => Math.random() - 0.5)
+    console.log(randomGalaxy);
 
-        console.log(randomGalaxy)
+    const ElementDad = document.getElementById("ElementDad");
 
-        const ElementDad = document.getElementById('ElementDad');
+    randomGalaxy.forEach((galaxy) => {
+      const myElement = document.createElement("div");
 
-        randomGalaxy.forEach(galaxy => {
-
-            const myElement = document.createElement('div');
-
-            myElement.innerHTML = `
+      myElement.innerHTML = `
             <div class="cards"> <!-- Div que contém o resultado -->
             <div class="image">
                 <img id="imagem" src="${galaxy.image}" alt=""> <!-- Imagem do resultado -->
@@ -23,25 +21,24 @@ fetch("galaxias.json")
             <h3 class="title">${galaxy.title}</h3> <!-- Título da imagem -->
 
             </div>
-            `
+            `;
 
-            ElementDad.appendChild(myElement);
+      ElementDad.appendChild(myElement);
 
-            myElement.addEventListener('click', () =>{
+      myElement.addEventListener("click", () => {
+        window.location.href = `galaxye-example.html?id=${galaxy.id}`;
+      });
 
-                window.location.href = `galaxye-example.html?id=${galaxy.id}`;
-            })
+      /* ========= Tratando o menu dropdown ========= */
+      // Seleciona todos os elementos com a classe "resultado"
+      var elementosResultado = document.querySelectorAll(".cards");
 
-            /* ========= Tratando o menu dropdown ========= */
-            // Seleciona todos os elementos com a classe "resultado"
-            var elementosResultado = document.querySelectorAll('.cards');
-
-            // Para cada elemento com a classe "resultado", adiciona um ouvinte de evento de clique
-             elementosResultado.forEach(function (cards) { 
-                // Quando um elemento com a classe "resultado" é clicado, é adicionado a classe "ativa"
-                cards.addEventListener('click', function () {
-                    cards.classList.toggle('ativa');
-                });
-            });
+      // Para cada elemento com a classe "resultado", adiciona um ouvinte de evento de clique
+      elementosResultado.forEach(function (cards) {
+        // Quando um elemento com a classe "resultado" é clicado, é adicionado a classe "ativa"
+        cards.addEventListener("click", function () {
+          cards.classList.toggle("ativa");
         });
-    })  
+      });
+    });
+  });
--- js/galaxye-example.js
+++ js/galaxye-example.js
@@ -1,23 +1,24 @@
 document.addEventListener("DOMContentLoaded", function () {
-    
-    const params = new URLSearchParams(window.location.search);
-    const galaxyId = params.get('id');
+  const params = new URLSearchParams(window.location.search);
+  const galaxyId = params.get("id");
 
-    const pai = document.getElementById('pai');
+  const pai = document.getElementById("pai");
 
-    fetch("galaxias.json")
-        .then((result) => result.json())
-        .then((result) => {
-            // Encontrar a galáxia com o ID correspondente
-            const galaxySelected = result.galaxias.find(galaxy => galaxy.id === galaxyId);
+  fetch("galaxias.json")
+    .then((result) => result.json())
+    .then((result) => {
+      // Encontrar a galáxia com o ID correspondente
+      const galaxySelected = result.galaxias.find(
+        (galaxy) => galaxy.id === galaxyId
+      );
 
-            // Fazer algo com a galáxia selecionada
-            console.log(galaxySelected);
+      // Fazer algo com a galáxia selecionada
+      console.log(galaxySelected);
 
-            // Aqui você pode exibir os detalhes da galáxia selecionada na página
-            // Por exemplo, atualizar o HTML para mostrar as informações da galáxia
-            const detailsElement = document.getElementById('pai');
-            detailsElement.innerHTML = `
+      // Aqui você pode exibir os detalhes da galáxia selecionada na página
+      // Por exemplo, atualizar o HTML para mostrar as informações da galáxia
+      const detailsElement = document.getElementById("pai");
+      detailsElement.innerHTML = `
             <div class="title">
             <h1>
               ${galaxySelected.title}
@@ -33,8 +34,8 @@ document.addEventListener("DOMContentLoaded", function () {
           </h1>
         </div>
             `;
-        })
-        .catch((error) => {
-            console.error('Erro ao obter os dados das galáxias:', error);
-        });
+    })
+    .catch((error) => {
+      console.error("Erro ao obter os dados das galáxias:", error);
+    });
 });
